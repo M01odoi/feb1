6de8f3412114901adb089ecf618e2fc40c180c60
Alex <alexsveshkarev@gmail.com>
2022-02-01 14:23:17 +0200

    refactored class Input

--- src/input.js
+++ src/input.js
@@ -4,7 +4,6 @@ export default class Input extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: this.props.value };
-    this.onChange = this.onChange.bind(this);
   }
 onChange = (event) =>{
     console.log(this.state.value);
